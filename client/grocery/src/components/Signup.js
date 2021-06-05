36fae2873d0ee6249ae573922ea2ad1d7ea87337
Asad Faraz <asadfaraz44@gmail.com>
2021-06-05 16:33:44 +0500

    signup & login pages contents changed

--- client/grocery/src/components/Signup.js
+++ client/grocery/src/components/Signup.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 const Signup = () => {
+  // form submitting
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("Sign up!!!");
+  };
+
   return (
     <div>
-      <h2>Signup Page</h2>
+      <h1>Signup Page</h1>
+
+      <form onSubmit={submitForm}>
+        <input type="text" placeholder="Full Name" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="City" />
+        <input type="password" placeholder="Password" />
+        <input type="submit" value="Signup" />
+      </form>
     </div>
   );
 };
