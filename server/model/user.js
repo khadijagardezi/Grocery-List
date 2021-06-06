725072101722a6d7e464615eb7349bcd1814f9ac
Asad Faraz <asadfaraz44@gmail.com>
2021-06-06 08:53:59 +0500

    users schema

---
+++ server/model/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: email,
+        required: true,
+    }, 
+
+    city: {
+        type: String,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+    
+    {
+        timestamps: {createdAt: "created_at", updatedAt: "updated_at"}
+    }
+
+});
+
+module.exports = mongoose.model("User", userSchema)
