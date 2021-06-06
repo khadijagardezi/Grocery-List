2218f493b410975ef7bf610d9b33f047d68fa0f1
Masood Akhtar <masoodwayfarer@gmail.com>
2021-06-06 19:11:57 +0500

    database added

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 const cors = require("cors");
 
@@ -8,5 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
+makeConnection();
 
-app.listen(3001, console.log("Server Started!!"));
+async function makeConnection() {
+  await mongoose.connect(
+    "mongodb://127.0.0.1:27017/GroceryApp?gssapiServiceName=mongodb",
+    { useNewUrlParser: true },
+    () => {
+      console.log("Database Connected!");
+      app.listen(3001, console.log("Server Started!!"));
+    }
+  );
+}
---
+++ server/model/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+module.exports = mongoose.model("Users", userSchema);
